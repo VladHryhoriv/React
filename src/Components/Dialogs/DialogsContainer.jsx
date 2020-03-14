f0decf52bd2ba9efdae902bd5aa577617c2959f3
VitaliyHr <grigorivv71@gmail.com>
2020-03-14 19:34:47 +0200

    Component did mount

--- src/Components/Dialogs/DialogsContainer.jsx
+++ src/Components/Dialogs/DialogsContainer.jsx
@@ -5,32 +5,6 @@ import Dialogs from './Dialogs';
 import { messageChangeActionCreator, sendMessageActionCreator } from '../../redux/dialogs-reducer';
 import { connect } from 'react-redux';
 
-
-// const DialogsContainer = (props) => {
-
-//     return <StoreContext.Consumer>{
-//             (store) => {
-//                 let dialogName = store.getState().dialogPage.dialogData.map((d) => <DialogItem id={d.id} name={d.name} />);
-//                 let messages = store.getState().dialogPage.messages.map(m => <Message message={m.text} />);
-//                 let newMassage = store.getState().dialogPage.newMassage;
-//                 let sendMassages = () => {
-//                     store.dispatch(sendMessageActionCreator());
-//                 }
-//                 let messageChange = (newtxt) => {
-//                     store.dispatch(messageChangeActionCreator(newtxt));
-//                 }
-//                 return (
-//                     <Dialogs //dialogName={dialogName} 
-//                     //messages={messages} 
-//                     sendMessageAction={sendMassages} 
-//                     //newMassage={newMassage} 
-//                     messageChangeAction={messageChange} />
-//                 )
-//             }
-//         }
-//     </StoreContext.Consumer>
-// }
-
 const mapStateToProps = (state) => {
     return{
         dialogName:state.dialogPage.dialogData.map((d) => <DialogItem id={d.id} key={d.id} name={d.name} />),
--- src/Components/Users/Usrers.jsx
+++ src/Components/Users/Usrers.jsx
@@ -3,44 +3,43 @@ import style from './Users.module.css';
 import * as axios from 'axios';
 import userPhoto from '../../assmeut/UserPhoto/user.png'
 
-const Users = (props) => {
-    if (props.users.length === 0) {
+class Users extends React.Component{
+    componentDidMount(){
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response=>{
-            props.setUsers(response.data.items);
+            this.props.setUsers(response.data.items);
         })
     }
-    console.log(props.users)
-    return (
-        <div className={style.wrapper}>
-            {
-                props.users.map(u => <div key={u.id}>
-                    <span>
+    render(){
+        return <div className={style.wrapper}>
+        {
+            this.props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        {console.log(u)}
+                            <img src={u.userPhoto!=null?null:userPhoto} alt="" srcset="" />
+                        </div>
                         <div>
-                            {console.log(u)}
-                                <img src={u.userPhoto!=null?null:userPhoto} alt="" srcset="" />
-                            </div>
-                            <div>
-                                {u.followed
-                                    ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                                    : <button onClick={() => { props.follow(u.id) }}>Follow</button>}
-                            </div>
-                    </span>
-                    <span>
-                    <span>
-                        {console.log()}
-                                <div>{u.name}</div>
-                                <div>{u.status}</div>
-                            </span>
-                            <span>
-                                <div>{"u.location.country"}</div>
-                                <div>{"u.location.city "}</div>
-                            </span>
-                    </span>
-                    </div>
-                )
-            }
-        </div>
-    )
+                            {u.followed
+                                ? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+                                : <button onClick={() => { this.props.follow(u.id) }}>Follow</button>}
+                        </div>
+                </span>
+                <span>
+                <span>
+                    {console.log()}
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                        <span>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city "}</div>
+                        </span>
+                </span>
+                </div>
+            )
+        }
+    </div>
+    }
 }
 
 export default Users;
